b407a511e155c4b959db52934b2dfbf8c04a7b4d
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-29 18:28:46 +0000

    Added Login/Signup Views and Logic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,60 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "password_security"
   end
 
+  get '/' do
+    erb :homepage
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect '/teams'
+    else
+      erb :login
+    end
+  end
+
+  post '/login' do
+    user = User.find_by(:username => params[:username])
+
+  	 if user && user.authenticate(params[:password])
+  			 session[:user_id] = user.id
+  			 redirect '/teams'
+  	 else
+  			 redirect "/failure"
+  	 end
+  end
+
+  get "/signup" do
+    if logged_in?
+      redirect "/teams"
+    else
+      erb :signup
+    end
+  end
+
+  post "/signup" do
+    if(params[:username] != "" && params[:password] != "")
+      user = User.new(:username => params[:username], :password => params[:password])
+    else
+      redirect "/signup"
+    end
+		if user.save
+       session[:user_id] = user.id
+       redirect "/teams"
+		else
+			 redirect "/failure"
+		end
+  end
+
+  get "/failure" do
+    erb :failure
+  end
+
+  get "/logout" do
+    session.clear
+    redirect "/login"
+  end
+
   helpers do
     def logged_in?
       !!session[:user_id]
--- app/models/team.rb
+++ app/models/team.rb
@@ -1,3 +1,15 @@
 class Team < ActiveRecord::Base
   belongs_to :user
+
+  def slug
+    self.username.downcase.gsub(" ", "-")
+  end
+
+  def self.find_by_slug(slug)
+    User.all.each do |user|
+      if(user.slug == slug)
+        return user
+      end
+    end
+  end
 end
