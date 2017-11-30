72e2b2a19a79d6738673daed0e0fa694a55b6474
Dembe Stephanos <stephanos@goldmail.etsu.edu>
2017-11-30 16:09:07 +0000

    Added Show Page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,4 +2,18 @@ require 'pry'
 
 class TeamsController < ApplicationController
 
+  configure do
+    set :public_folder, 'public'
+    set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/teams' do
+    @user = current_user
+    @teams = Team.all
+
+    erb :'teams/teams'
+  end
+
 end
